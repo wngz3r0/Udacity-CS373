6b932c68de208d5a74df2a38306f4d427212bdfb
me@wngz3r0.info <me@wngz3r0.info>
2012-04-16 22:47:08 -0700

    Adding python 2.7 version

--- unit1_quiz1.py
+++ unit1_quiz1.py
@@ -2,5 +2,6 @@ def get_probability(p,i):
     return p[i]
 
 num_boxes = 5
+# python2.7 version: p = map(lamda_:1.0 / num_boxes, range(num_boxes))
 p = list(map(lambda _: 1 / num_boxes, range(num_boxes)))
 print (get_probability(p,3))
